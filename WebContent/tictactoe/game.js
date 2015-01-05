ff2cf09a42f28140d4e33b548ffe3cbd597a17d3
Matej Risek <mrisek@gmail.com>
2015-01-05 16:08:48 +0100

    Client updated.

--- WebContent/tictactoe/game.js
+++ WebContent/tictactoe/game.js
@@ -1,96 +1,160 @@
+function createMatrix(x, y) {
+    var matrix = [];
+    for (i = 0; i < x; i++) {
+        matrix[i] = new Array(y);
+    }
+    return matrix;
+}
+
 function relMouseCoords(canvas, evt) {
-	var rect = canvas.getBoundingClientRect();
-	return {
-		x : evt.clientX - rect.left,
-		y : evt.clientY - rect.top
-	};
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
 }
 
 HTMLCanvasElement.prototype.relMouseCoords = relMouseCoords;
 
 function canvasClick(event) {
-	var tile = calculateTile(this.relMouseCoords(this, event),
-			canvas.noHrTiles, canvas.noVrTiles);
-	var putMark = {
-		code : 'put',
-		coords : {
-			x : tile.x,
-			y : tile.y
-		}
-	};
-	socket.send(JSON.stringify(putMark));
+
+    var tile = calculateTile(this.relMouseCoords(this, event),
+        canvas.noHrTiles, canvas.noVrTiles);
+	if (!Game.State.isValid(tile.x, tile.y)) {
+        var textPopup = new PopupBox(canvas, "Invalid move", 750);
+        textPopup.show();
+		return false;
+	}
+    var putMark = {
+        code: 'put',
+        coords: {
+            x: tile.x,
+            y: tile.y
+        }
+    };
+    Game.socket.send(JSON.stringify(putMark));
 }
 
 var canvas = document.getElementById('myCanvas');
 
-var messageParser = {};
-messageParser.parse = function(message) {
-	var obj = JSON.parse(message);
-	switch (obj.code) {
-	case 'put':
-		var mark = obj.mark;
-		if (mark === 'x') {
-			console.log('drawing X');
-			drawX(canvas, calculateTile(obj.coords, canvas.noHrTiles,
-					canvas.noVrTiles, true));
-		} else {
-			console.log('drawing O');
-			drawO(canvas, calculateTile(obj.coords, canvas.noHrTiles,
-					canvas.noVrTiles, true));
+var Game = {
+    ws: {},
+    messageParser: {}
+};
+
+Game.State = {};
+
+Game.State.isValid = function(x, y) {
+    if (Game.State.board != 'undefined') {
+        return true;
+    }
+    return false;
+}
+
+Game.State.put = function(x, y, mark) {
+    Game.State.board[x][y] = mark;
+}
+
+Game.State.init = function(noHrTiles, noVrTiles) {
+    var board = createMatrix(3, 3);
+    return board;
+}
+
+Game.State.render = function() {
+	canvas.getContext('2d').clearRect ( 0 , 0 , canvas.width, canvas.height );
+	drawGrid(canvas, canvas.noHrTiles, canvas.noVrTiles);
+	var board = Game.State.board;
+	var hr = board.length;
+	var vr = board[0].length;
+	for (x = 0; x < hr; x++) {
+		for (y = 0; y < vr; y++) {
+			var mark = board[x][y];
+			switch (mark) {
+				case 'x':
+					drawX(canvas, calculateTile({x: x, y: y}, canvas.noHrTiles,
+                    canvas.noVrTiles, true));
+					break;
+				case 'o':
+					drawO(canvas, calculateTile({x: x, y: y}, canvas.noHrTiles,
+                    canvas.noVrTiles, true));
+                    break;
+				default:
+
+			}
 		}
-		break;
-
-	case 'start':
-		Game.startPopup.hide();
-		var gameStartPopup = new PopupBox(canvas, "You're playing as "
-				+ obj.mark, 750);
-		gameStartPopup.show();
-		break;
-
-	case 'restart':
-		Game.init(3, 3);
-		break;
-		
-	case 'msg':
-		var textPopup = new PopupBox(canvas, obj.text, 750);
-		textPopup.show();
-		break;
 	}
 }
 
-var Game = {}
-Game.init = function(noHrTiles, noVrTiles) {
-	canvas.noHrTiles = noHrTiles;
-	canvas.noVrTiles = noVrTiles;
-	// Init game
-	canvas.width = 640;
-	canvas.height = 640;
-	drawGrid(canvas, noHrTiles, noVrTiles);
-	this.startPopup = new PopupBox(canvas, "Waiting for opponent", 0);
-	this.startPopup.show();
+Game.init = function(host, noHrTiles, noVrTiles, restart) {
+    canvas.noHrTiles = noHrTiles;
+    canvas.noVrTiles = noVrTiles;
+    // Init game
+    canvas.width = 640;
+    canvas.height = 640;
+    // drawGrid(canvas, noHrTiles, noVrTiles);
+    Game.State.board = [];
+    Game.State.board = Game.State.init(noHrTiles, noVrTiles);
+    console.log('Rendering new game');
+    Game.State.render();
+    this.startPopup = new PopupBox(canvas, "Waiting for opponent", 0);
+    this.startPopup.show(this);
+    if (restart)
+    	this.socket = this.ws.init(host);
+
 }
 
-Game.init(3, 3);
+Game.ws.init = function(host) {
+    // WebSocket
+    var host = host;
+    var socket = null;
+    if ('WebSocket' in window) {
+        socket = new WebSocket(host);
+    } else if ('MozWebSocket' in window) {
+        socket = new MozWebSocket(host);
+    } else {
+        console.log('Error: WebSocket is not supported by this browser.');
+    }
 
-// WebSocket
-var host = 'ws://' + window.location.host + '/AMZavrsni/tictactoe/ws';
-var socket = null;
-if ('WebSocket' in window) {
-	socket = new WebSocket(host);
-} else if ('MozWebSocket' in window) {
-	socket = new MozWebSocket(host);
-} else {
-	console.log('Error: WebSocket is not supported by this browser.');
+    socket.onopen = function() {
+        console.log('Info: WebSocket connection opened.');
+    };
+
+    socket.onclose = function() {};
+
+    socket.onmessage = function(message) {
+        console.log('Message: ' + message.data);
+        Game.messageParser.parse(message.data);
+    };
+
+    return socket;
 }
 
-socket.onopen = function() {
-	console.log('Info: WebSocket connection opened.');
-};
+Game.messageParser.parse = function(message) {
+    var obj = JSON.parse(message);
+    switch (obj.code) {
+        case 'put':
+            Game.State.put(obj.coords.x, obj.coords.y, obj.mark);
+            Game.State.render();
+            break;
 
-socket.onclose = function() {
-};
+        case 'start':
+            Game.init(host, 3, 3, false);
+            Game.startPopup.hide();
+            var gameStartPopup = new PopupBox(canvas, "You're playing as " + obj.mark, 750);
+            gameStartPopup.show(Game);
+            break;
 
-socket.onmessage = function(message) {
-	console.log('Message: ' + message.data);
-	messageParser.parse(message.data);
-};
+        case 'restart':
+            Game.init(host, 3, 3, true);
+            break;
+
+        case 'msg':
+            var textPopup = new PopupBox(canvas, obj.text, 750);
+            textPopup.show(Game);
+            break;
+    }
+}
+
+ var host = 'ws://' + window.location.host + '/AMZavrsni/tictactoe/ws';
+//var host = 'ws://' + 'localhost:8080' + '/AMZavrsni/tictactoe/ws';
+Game.init(host, 3, 3, true);
--- WebContent/tictactoe/graphics.js
+++ WebContent/tictactoe/graphics.js
@@ -1,126 +1,141 @@
+"use strict";
+
 function calculateTile(coords, noHrTiles, noVrTiles) {
-	calculateTile(coords, false);
+    calculateTile(coords, noHrTiles, noVrTiles, false);
 }
 
 function calculateTile(coords, noHrTiles, noVrTiles, fromMessage) {
-	var hrStep = canvas.width / noVrTiles;
-	var vrStep = canvas.height / noHrTiles;
+	console.log(coords);
+    var hrStep = canvas.width / noVrTiles;
+    var vrStep = canvas.height / noHrTiles;
 
-	if (!fromMessage) {
-		var hrTileCord = Math.floor(coords.x / hrStep);
-		var vrTileCord = Math.floor(coords.y / vrStep);
-	} else {
-		var hrTileCord = coords.x;
-		var vrTileCord = coords.y;
-	}
+    if (!fromMessage) {
+        var hrTileCord = Math.floor(coords.x / hrStep);
+        var vrTileCord = Math.floor(coords.y / vrStep);
+    } else {
+    	console.log('HERE');
+        var hrTileCord = coords.x;
+        var vrTileCord = coords.y;
+    }
+	var tile = {x: hrTileCord,
+        y: vrTileCord,
+        xStart: hrTileCord * hrStep,
+        xEnd: hrTileCord * hrStep + hrStep,
+        yStart: vrTileCord * vrStep,
+        yEnd: vrTileCord * vrStep + vrStep};
 
-	return {
-		x : hrTileCord,
-		y : vrTileCord,
-		xStart : hrTileCord * hrStep,
-		xEnd : hrTileCord * hrStep + hrStep,
-		yStart : vrTileCord * vrStep,
-		yEnd : vrTileCord * vrStep + vrStep
-	};
+    return {
+        x: hrTileCord,
+        y: vrTileCord,
+        xStart: hrTileCord * hrStep,
+        xEnd: hrTileCord * hrStep + hrStep,
+        yStart: vrTileCord * vrStep,
+        yEnd: vrTileCord * vrStep + vrStep
+    };
 }
 
 function drawX(canvas, coords) {
-	var ctx = canvas.getContext('2d');
-	ctx.beginPath();
-	ctx.moveTo(coords.xStart + 20, coords.yStart + 20);
-	ctx.lineTo(coords.xEnd - 20, coords.yEnd - 20);
-	ctx.moveTo(coords.xStart + 20, coords.yEnd - 20);
-	ctx.lineTo(coords.xEnd - 20, coords.yStart + 20);
-	ctx.lineWidth = 5;
-	ctx.strokeStyle = '#5380D4';
-	ctx.stroke();
+    var ctx = canvas.getContext('2d');
+    ctx.beginPath();
+    ctx.moveTo(coords.xStart + 20, coords.yStart + 20);
+    ctx.lineTo(coords.xEnd - 20, coords.yEnd - 20);
+    ctx.moveTo(coords.xStart + 20, coords.yEnd - 20);
+    ctx.lineTo(coords.xEnd - 20, coords.yStart + 20);
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = '#5380D4';
+    ctx.stroke();
 }
 
 function drawO(canvas, coords) {
-	var ctx = canvas.getContext('2d');
-	ctx.beginPath();
-	ctx.arc((coords.xStart + coords.xEnd) / 2,
-			(coords.yStart + coords.yEnd) / 2, coords.yEnd
-					- (coords.yStart + coords.yEnd) / 2 - 20, 0, 2 * Math.PI,
-			false);
-	ctx.lineWidth = 5;
-	ctx.strokeStyle = '#FA021B';
-	ctx.stroke();
+    var ctx = canvas.getContext('2d');
+    ctx.beginPath();
+    ctx.arc((coords.xStart + coords.xEnd) / 2, (coords.yStart + coords.yEnd) / 2, coords.yEnd - (coords.yStart + coords
+            .yEnd) / 2 - 20, 0, 2 * Math.PI,
+        false);
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = '#FA021B';
+    ctx.stroke();
 }
 
 function drawGrid(canvas, noHrTiles, noVrTiles) {
-	var x = 0;
-	var y = 0;
-	var maxWidth = canvas.width;
-	var maxHeight = canvas.height;
-	var hrStep = canvas.width / noHrTiles;
-	var vrStep = canvas.height / noVrTiles;
+    var x = 0;
+    var y = 0;
+    var maxWidth = canvas.width;
+    var maxHeight = canvas.height;
+    var hrStep = canvas.width / noHrTiles;
+    var vrStep = canvas.height / noVrTiles;
 
-	var ctx = canvas.getContext('2d');
-	ctx.strokeStyle = '#000000';
-	// Horizontal lines
-	for (var i = 0; i < noHrTiles + 1; i++) {
-		ctx.moveTo(x, y);
-		ctx.lineTo(maxWidth, y);
-		ctx.lineWidth = 3;
-		ctx.stroke();
-		y += hrStep;
-	}
+    var ctx = canvas.getContext('2d');
+    ctx.strokeStyle = '#000000';
 
-	y = 0;
+    // Horizontal lines
+    for (var i = 0; i < noHrTiles + 1; i++) {
+        ctx.moveTo(x, y);
+        ctx.lineTo(maxWidth, y);
+        ctx.lineWidth = 3;
+        ctx.stroke();
+        y += hrStep;
+    }
 
-	// Vertical lines
-	for (var i = 0; i < noVrTiles + 1; i++) {
-		ctx.moveTo(x, y);
-		ctx.lineTo(x, maxHeight);
-		ctx.stroke();
-		x += vrStep;
-	}
+    y = 0;
+
+    // Vertical lines
+    for (var i = 0; i < noVrTiles + 1; i++) {
+        ctx.moveTo(x, y);
+        ctx.lineTo(x, maxHeight);
+        ctx.lineWidth = 3;
+        ctx.stroke();
+        x += vrStep;
+    }
 }
 
 function getScaledFont(canvas) {
-	return 'bold ' + canvas.width / 10 + 'px Arial ';
+    return 'bold ' + canvas.width / 10 + 'px Arial ';
 }
 
 function PopupBox(canvas, text, timeout) {
-	this.canvas = canvas;
-	this.ctx = canvas.getContext('2d');
-	this.imgData = null;
-	this.timeout = timeout;
+    this.canvas = canvas;
+    this.ctx = canvas.getContext('2d');
+    this.imgData = null;
+    this.timeout = timeout;
 
-	this.draw = function() {
-		var ctx = this.ctx;
-		ctx.beginPath();
-		ctx.rect(0, canvas.height / 3, canvas.width, canvas.height / 3);
-		ctx.fillStyle = 'white';
-		ctx.fill();
-		ctx.strokeStyle = 'black';
-		ctx.stroke();
+    this.draw = function() {
+        var ctx = this.ctx;
+        ctx.beginPath();
+        ctx.rect(0, canvas.height / 3, canvas.width, canvas.height / 3);
+        ctx.fillStyle = 'white';
+        ctx.fill();
+        ctx.strokeStyle = 'black';
+        ctx.stroke();
 
-		ctx.font = getScaledFont(canvas);
-		ctx.textAlign = 'center';
-		ctx.fillStyle = 'black';
-		ctx.fillText(text, canvas.width / 2, canvas.height / 3 + canvas.height
-				/ 5.8);
-	}
+        ctx.font = getScaledFont(canvas);
+        ctx.textAlign = 'center';
+        ctx.fillStyle = 'black';
+        ctx.fillText(text, canvas.width / 2, canvas.height / 3 + canvas.height / 5.8);
+    }
 
 }
 
-PopupBox.prototype.hide = function() {
-	if (this.imgData != null) {
-		this.ctx.putImageData(this.imgData, 0, 0);
-		this.canvas.addEventListener("click", canvasClick);
+PopupBox.prototype.hide = function(game) {
+	if (game != null) {
+		console.log('Calling game render');
+		Game.State.render();
+		this.canvas.addEventListener('click', canvasClick);
 	}
+    else if (this.imgData != null) {
+        // this.ctx.putImageData(this.imgData, 0, 0);
+        this.canvas.addEventListener('click', canvasClick);
+    }
 }
 
-PopupBox.prototype.show = function() {
-	var popup = this;
-	this.canvas.removeEventListener("click", canvasClick);
-	this.imgData = this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
-	this.draw();
-	if (this.timeout > 0) {
-		setTimeout(function() {
-			popup.hide();
-		}, this.timeout);
-	}
+PopupBox.prototype.show = function(game) {
+    var popup = this;
+    this.canvas.removeEventListener('click', canvasClick);
+    this.imgData = this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
+    this.draw();
+    if (this.timeout > 0) {
+        setTimeout(function() {
+            popup.hide(game);
+        }, this.timeout);
+    }
 }
